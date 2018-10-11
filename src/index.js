2184bbb167ddd5142af4bb2a08bdb1deef3cde96
Caleb Tomlin <tkmaj9x@is-c02wf0glhv2g.cp.ad.kohls.com>
2018-10-11 08:49:26 -0500

    changed file layout

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from "react-dom";
 import Main from "./Main";
 import "./index.css";
 
-// Check that service workers are registered
-if ('serviceWorker' in navigator) {
-  // Use the window load event to keep the page load performant
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
-  });
-}
 ReactDOM.render(
   <Main/>, 
   document.getElementById("root")
