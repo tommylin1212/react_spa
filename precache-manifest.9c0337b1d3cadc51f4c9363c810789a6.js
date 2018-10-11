d27f880ebaf27305e7903e0962ed46e0bf9b0f3a
Caleb Tomlin <tkmaj9x@is-c02wf0glhv2g.cp.ad.kohls.com>
2018-10-11 09:07:47 -0500

    Updates

---
+++ precache-manifest.9c0337b1d3cadc51f4c9363c810789a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "678fd35739c62777f82249cc931ec9ac",
+    "url": "/react_spa/static/media/largeimage.678fd357.jpg"
+  },
+  {
+    "revision": "e69d20ec678e3184859c",
+    "url": "/react_spa/static/js/runtime~main.e69d20ec.js"
+  },
+  {
+    "revision": "a64357e1bc9a45d0e954",
+    "url": "/react_spa/static/js/main.a64357e1.chunk.js"
+  },
+  {
+    "revision": "9b7dad2895115e772bb4",
+    "url": "/react_spa/static/js/1.9b7dad28.chunk.js"
+  },
+  {
+    "revision": "a64357e1bc9a45d0e954",
+    "url": "/react_spa/static/css/main.1958dad7.chunk.css"
+  },
+  {
+    "revision": "92cc9d9867933ab050bdccba8f72217d",
+    "url": "/react_spa/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/react_spa/precache-manifest.b596cc525bae97c1d460842dabda5620.js"
+  "/react_spa/precache-manifest.9c0337b1d3cadc51f4c9363c810789a6.js"
 );
 
 workbox.clientsClaim();
