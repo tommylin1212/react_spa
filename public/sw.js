6daa0c10a8c7c4ba831600d5eb469415a0f4faf8
Caleb Tomlin <tkmaj9x@is-c02wf0glhv2g.cp.ad.kohls.com>
2018-10-11 09:07:28 -0500

    moved things

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,12 @@
-console.log("wazzup");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+workbox.routing.registerRoute(
+    new RegExp('.*'),
+    workbox.strategies.networkFirst()
+  );
