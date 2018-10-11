0872073701ca9d1ccea9905891da15d3f58debbb
Caleb Tomlin <tkmaj9x@is-c02wf0glhv2g.cp.ad.kohls.com>
2018-10-11 08:40:21 -0500

    test

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+class Home extends Component {
+    render() {
+      return (
+        <div>
+          <h2>HELLO</h2>
+          <p>Hello World</p>
+        </div>
+      );
+    }
+  }
+  export default Home;
---
+++ src/Stuff.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+ 
+class Stuff extends Component {
+  render() {
+    return (
+      <div>
+        <h2>STUFF</h2>
+        <p>Wow so much stuff.</p>
+      </div>
+    );
+  }
+}
+export default Stuff;
