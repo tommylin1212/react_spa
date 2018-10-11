0b31d1c64809ae3b4247c833c6c65666a34d1cfe
Caleb Tomlin <tkmaj9x@is-c02wf0glhv2g.cp.ad.kohls.com>
2018-10-11 08:53:37 -0500

    Updates

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-console.log("wazzup");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
